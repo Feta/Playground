688591515206587761a1359e9a79554a4175eead
Feta <konstantinos.filippakos@uconn.edu>
2018-06-07 13:05:25 -0400

    So pat can see it

--- Ninite methods.ps1
+++ Ninite methods.ps1
@@ -1,6 +1,6 @@
 <#(Invoke-WebRequest -Uri "https://www.jetbrains.com/toolbox/download/download-thanks.html").Links 
 #Invoke-WebRequest –Uri https://www.jetbrains.com/toolbox/download/download-thanks.html
-((Invoke-WebRequest -Uri "https://www.jetbrains.com/toolbox/download/download-thanks.html").Links | Where-Object class -eq “download-link”).Title#>
+((Invoke-WebRequest -Uri "https://www.jetbrains.com/toolbox/download/download-thanks.html").Links | Where-Object class -eq “download-link”).Title
 
 $jetbrains = Invoke-WebRequest -Uri https://www.jetbrains.com/toolbox/download/download-thanks.html
 #$jetbrains | Get-Member -MemberType Property
@@ -9,10 +9,26 @@ $jetbrains = Invoke-WebRequest -Uri https://www.jetbrains.com/toolbox/download/d
 #$jetbrains.AllElements | Get-Member
 #$jetbrains.AllElements | Select-Object -ExpandProperty tagname -Unique
 $jetbrains.ParsedHtml.all | Select-Object ID -Unique
-#$jetbrains.ParsedHtml.all | Select-Object download-link
-$jetbrainsdownload = $jetbrains.ParsedHtml.all | Where-Object {$_.href -like "https*"} | Where-Object ID -eq "download-link".Title
+$jetbrains.ParsedHTML.GetElementByID('download-link') | Select-Object -Property id
+$url = 'https://www.jetbrains.com/toolbox/download/download-thanks.html'
+$jetbrains2 = (Invoke-WebRequest -Uri $url).Links | Where-Object href -like *exe*
+$jetbrains2 | Select-Object -Unique href | ForEach-Object { 
+    #get file name
+    $name = $jetbrains2 | Where-Object href -eq $_.href  | Select-Object -First 1 -ExpandProperty innerHtml 
+        
+        
+    "going to DL $name"
+        
+    #get actual DL link
 
-Start-BitsTransfer -Source $jetbrainsdownload -Destination C:\Users\Kosta\Desktop
+    $link = Invoke-WebRequest $_.href  | Select-Object -ExpandProperty Links | Where-Object href -Like *exe | Select-Object -ExpandProperty href
+    
+    timeout 5
+        
+    Invoke-WebRequest -Uri $link  -OutFile C:\Users\Kosta\Desktop -Verbose 
+}        
+
+
 
 
 
@@ -22,4 +38,18 @@ Start-BitsTransfer -Source $jetbrainsdownload -Destination C:\Users\Kosta\Deskto
 $mylinks | out-string -Width 400
 
 # output to file
-$mylinks | out-string -Width 400 > 'C:\Users\Kosta\Desktop\Coding Env\Playground'#>
+$mylinks | out-string -Width 400 > 'C:\Users\Kosta\Desktop\Coding Env\Playground'#>#>
+
+
+$jetbrains = Invoke-WebRequest -Uri https://www.jetbrains.com/toolbox/download/download-thanks.html
+$jetbrains.ParsedHtml.getElementsByTagName('p') |
+    Where-Object { $_.className -eq 'sub-title no-margin-bottom' } |
+    ForEach-Object { $_.getElementsByTagName('download-link') } |
+    Select-Object -Expand href
+
+$jetbrains.Links |
+    Where-Object { $_.ID -eq 'download-link' } |
+    Select-Object -ExpandProperty href
+    $jetbrainsdownload = Select-Object -ExpandProperty href
+    Write-Host $jetbraindownload.toString()
+    Start-BitsTransfer -Source $jetbrainsdownload -Destination C:\Users\Kosta\Desktop
